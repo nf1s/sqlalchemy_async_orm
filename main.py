2a0fbadeb93014e71ecdf1c1f55915f61ce7547c
Ahmed Nafies <ahmed.nafies@gmail.com>
2021-05-23 18:20:12 +0200

    cleans

--- main.py
+++ main.py
@@ -9,19 +9,29 @@ async def init_app():
     await async_db_session.create_all()
 
 
-async def async_main():
-    await init_app()
+async def create_user():
     await User.create(full_name="John Doe")
     user = await User.get(1)
-    print(user.id)
-    print(user.full_name)
+    return user.id
 
-    posts = await Post.filter_by_user_id(user.id)
-    print(posts)
 
-    await User.update(id=1, full_name="John Not Doe")
-    user = await User.get(1)
-    print(user.full_name)
+async def create_post(user_id, data):
+    await Post.create(user_id=user_id, data=data)
+    posts = await Post.filter_by_user_id(user_id)
+    return posts
+
+
+async def update_user(id, full_name):
+    await User.update(id, full_name="John Not Doe")
+    user = await User.get(id)
+    return user.full_name
+
+
+async def async_main():
+    await init_app()
+    user_id = await create_user()
+    await update_user(user_id, "John Not Doe")
+    await create_post(user_id, "hello world")
 
 
 asyncio.run(async_main())
